de95159b262aaf69ccf1a198e8ded6c237179d3f
최원진 <>
2019-10-07 00:06:59 +0900

    환경 변수 변경

--- index.js
+++
@@ -1,8 +0,0 @@
-//import "./src/env";
-import server from "./src/server";
-
-const port = process.env.PORT || 4000;
-
-server.start({ port }, () =>
-  console.log(`✅ Server running on http://localhost:${port}`)
-);
--- src/server.js
+++ src/server.js
@@ -1,7 +1,9 @@
+import './env'
 import { GraphQLServer } from "graphql-yoga";
 import logger from "morgan";
 import schema from "../graphql/schema";
 import {authenticateJwt,isAuthenticated} from "../auth/jwt"
+
 const server = new GraphQLServer({
   schema,
   context:({request})=>({request,isAuthenticated})
@@ -9,4 +11,10 @@ const server = new GraphQLServer({
 
 server.express.use(logger("dev"));
 server.express.use(authenticateJwt);
-export default server;
+
+
+const port = process.env.PORT || 4000;
+server.start({ port }, () =>
+  console.log(`✅ Server running on http://localhost:${port}`)
+);
+
