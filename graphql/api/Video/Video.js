cc8695bb723598a8ac94a00952ef706502d8b515
최원진 <>
2019-10-06 23:20:01 +0900

    타임링크 생성 graphql 수정

--- graphql/api/Video/Video.js
+++ graphql/api/Video/Video.js
@@ -2,8 +2,8 @@ import { prisma } from "../../../prisma/generated/prisma-client";
 
 export default {
   Video: {
-    schedule: async({ id }) => await prisma.video({ id }).schedule(),
-    chats: async({ id }) =>await prisma.video({ id }).chats(),
-    timeLinks:async({ id }) =>await prisma.video({ id }).timeLinks(),
+    schedule: ({ id }) =>  prisma.video({ id }).schedule(),
+    chats: ({ id }) => prisma.video({ id }).chats(),
+    timeLinks:({ id }) => prisma.video({ id }).timeLinks(),
   }
 };
