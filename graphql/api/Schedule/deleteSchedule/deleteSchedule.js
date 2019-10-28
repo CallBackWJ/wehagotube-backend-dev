170f277d053f39a8f7e61c80e03ffec461001020
최원진 <>
2019-10-28 14:55:04 +0900

    스케줄 삭제 추가

--- graphql/api/Schedule/deleteSchedule/deleteSchedule.js
+++ graphql/api/Schedule/deleteSchedule/deleteSchedule.js
@@ -1,9 +1,16 @@
 import { prisma } from "../../../../prisma/generated/prisma-client";
 export default {
   Mutation: {
-    deleteSchedule: async (_, { id}) =>
-      await prisma.deleteSchedule({
-          id
-      })
+    deleteSchedule: async (_, { id }) => {
+      const tem = await prisma.schedule({ id });
+      if (tem.status === "RESERVED") {
+        await prisma.deleteSchedule({ id });
+        console.log("삭제성공")
+        return true;
+      } else {
+        console.log("삭제실패")
+        return false;
+      }
+    }
   }
 };
